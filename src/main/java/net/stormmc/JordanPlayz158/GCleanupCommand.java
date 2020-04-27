29784d8bab282098f24fd6eb48428a22cb267014
JordanPlayz158 <JordanPlayz158again@gmail.com>
2020-04-26 20:02:16 -0400

    Adding Debug Code

--- src/main/java/net/stormmc/JordanPlayz158/GCleanupCommand.java
+++ src/main/java/net/stormmc/JordanPlayz158/GCleanupCommand.java
@@ -19,6 +19,7 @@ public class GCleanupCommand implements CommandExecutor {
     public static GBackup plugin;
     // When the command is used, do the steps below
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        System.out.println(plugin.getConfig().getInt("DaysBeforeDeletion"));
         // Run the Cleanup method with required variable pulled from the config
         Cleanup(plugin.getConfig().getInt("DaysBeforeDeletion"));
         // You need to return true or false when using a boolean method
