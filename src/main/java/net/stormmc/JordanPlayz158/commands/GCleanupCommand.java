e3b68e68140b76ae4c300ad4f2529ec1c1b868ec
JordanPlayz158 <JordanPlayz158again@gmail.com>
2020-05-01 16:33:44 -0400

    Fixed /gcleanup Command

--- src/main/java/net/stormmc/JordanPlayz158/commands/GCleanupCommand.java
+++ src/main/java/net/stormmc/JordanPlayz158/commands/GCleanupCommand.java
@@ -5,8 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
 
 public class GCleanupCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -17,20 +16,18 @@ public class GCleanupCommand implements CommandExecutor {
     }
 
     public static void Cleanup(int DaysBeforeDeletion) {
-        // Variable for storing the strings/logs
-        String s;
-        // Variable for storing the process ID
-        Process p;
-        try {
-            // Tells the system to run the command
-            p = Runtime.getRuntime().exec("find backups/* -mtime +" + DaysBeforeDeletion +" -type f -delete");
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(p.getInputStream()));
-            while ((s = br.readLine()) != null)
-                System.out.println("line: " + s);
-            p.waitFor();
-            System.out.println ("exit: " + p.exitValue());
-            p.destroy();
-        } catch (Exception e) {}
+        File directory = new File("backups/");
+        if(directory.exists()){
+
+            File[] listFiles = directory.listFiles();
+            long purgeTime = System.currentTimeMillis() - (DaysBeforeDeletion * 24 * 60 * 60 * 1000);
+            for(File listFile : listFiles) {
+                if(listFile.lastModified() < purgeTime) {
+                    if(!listFile.delete()) {
+                        System.err.println("Unable to delete file: " + listFile);
+                    }
+                }
+            }
+        }
     }
 }
